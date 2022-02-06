be5737c8a132f336c4f5b311728c56c81a8b5ecd
Envadors <91122426+Envadors@users.noreply.github.com>
2022-02-06 08:04:31 +0300

    Not Dangerous But Activates Windows 8.1

---
+++ Windows 8.1 Activator.cmd
@@ -0,0 +1,5 @@
+mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""Please Wait While We activate Windows 8.1 For Free"")(window.close)")
+mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""Your PC Will Restart In A Few Moments"")(window.close)")
+slmgr /ipk GCRJD-8NW9H-F2CDX-CCM8D-9D6T9
+slmgr /skms s8.uk
+shutdown /r /c "Thank You For Chosing Us!"
